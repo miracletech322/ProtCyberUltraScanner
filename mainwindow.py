0ffae67142d2b75dfeca6d4e9ff378f9b4fc89f7
miracletech322 <miracle.tech322@gmail.com>
2026-01-22 07:26:05 +0200

    SettingWindow

--- mainwindow.py
+++ mainwindow.py
@@ -10,25 +10,63 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         
+        # Store references to subwindows
+        self.frontwindow_subwindow = None
+        self.settingwindow_subwindow = None
+        
         # Show frontwindow in mdiArea by default
         self.show_frontwindow()
     
     def show_frontwindow(self):
         """Create and show FrontWindow in the MDI area"""
-        front_window = FrontWindow(self)
-        # Pass reference to MainWindow so FrontWindow can show SettingWindow
-        front_window.main_window = self
-        sub_window = QMdiSubWindow()
-        sub_window.setWidget(front_window)
-        sub_window.setWindowFlags(Qt.FramelessWindowHint)
-        self.ui.mdiArea.addSubWindow(sub_window)
-        sub_window.showMaximized()
+        # Only create if it doesn't exist
+        if self.frontwindow_subwindow is None:
+            front_window = FrontWindow(self)
+            # Pass reference to MainWindow so FrontWindow can show SettingWindow
+            front_window.main_window = self
+            sub_window = QMdiSubWindow()
+            sub_window.setWidget(front_window)
+            sub_window.setWindowFlags(Qt.FramelessWindowHint)
+            self.ui.mdiArea.addSubWindow(sub_window)
+            self.frontwindow_subwindow = sub_window
+            # Prevent frontwindow from being closed - override closeEvent
+            def prevent_close(event):
+                event.ignore()
+            sub_window.closeEvent = prevent_close
+            sub_window.showMaximized()
+        else:
+            # If it exists, just show it
+            self.frontwindow_subwindow.showMaximized()
+            self.ui.mdiArea.setActiveSubWindow(self.frontwindow_subwindow)
     
     def show_settingwindow(self):
         """Create and show SettingWindow in the MDI area"""
-        setting_window = SettingWindow()
-        sub_window = QMdiSubWindow()
-        sub_window.setWidget(setting_window)
-        sub_window.setWindowFlags(Qt.FramelessWindowHint)
-        self.ui.mdiArea.addSubWindow(sub_window)
-        sub_window.show()
+        # Only create if it doesn't exist
+        if self.settingwindow_subwindow is None:
+            setting_window = SettingWindow()
+            # Pass reference to MainWindow so SettingWindow can close itself and show frontwindow
+            setting_window.main_window = self
+            sub_window = QMdiSubWindow()
+            sub_window.setWidget(setting_window)
+            sub_window.setWindowFlags(Qt.FramelessWindowHint)
+            self.ui.mdiArea.addSubWindow(sub_window)
+            self.settingwindow_subwindow = sub_window
+            # Connect close event to clear reference
+            sub_window.destroyed.connect(lambda: setattr(self, 'settingwindow_subwindow', None))
+            sub_window.showMaximized()
+            self.ui.mdiArea.setActiveSubWindow(sub_window)
+        else:
+            # If it exists, just show it
+            self.settingwindow_subwindow.showMaximized()
+            self.ui.mdiArea.setActiveSubWindow(self.settingwindow_subwindow)
+    
+    def close_settingwindow_and_show_frontwindow(self):
+        """Close settingwindow and show frontwindow"""
+        if self.settingwindow_subwindow:
+            self.settingwindow_subwindow.close()
+            self.settingwindow_subwindow = None
+        
+        # Show frontwindow
+        if self.frontwindow_subwindow:
+            self.frontwindow_subwindow.showMaximized()
+            self.ui.mdiArea.setActiveSubWindow(self.frontwindow_subwindow)
--- settingwindow.py
+++ settingwindow.py
@@ -1,3 +1,4 @@
+from PySide6.QtCore import QFile, QTextStream
 from PySide6.QtWidgets import QWidget
 from ui_settingwindow import Ui_SettingWindow
 
@@ -6,3 +7,97 @@ class SettingWindow(QWidget):
         super().__init__()
         self.ui = Ui_SettingWindow()
         self.ui.setupUi(self)
+        self.main_window = None  # Will be set by MainWindow
+        self.setup_tab_widget()
+        self.setup_toggle_buttons()
+        self.setup_back_button()
+        self.load_stylesheet()
+    
+    def setup_toggle_buttons(self):
+        """Configure navigation buttons as toggle buttons"""
+        # Make all navigation buttons checkable
+        buttons = [
+            self.ui.btnTests,
+            self.ui.btnProxy,
+            self.ui.btnAuthentication,
+            self.ui.btnHTTP,
+            self.ui.btnCrawler,
+            self.ui.btnFormInputs,
+            self.ui.btnTestVectors,
+            self.ui.btnTechnologies
+        ]
+        
+        for button in buttons:
+            button.setCheckable(True)
+            button.toggled.connect(lambda checked, btn=button: self.on_button_toggled(btn, checked))
+        
+        # Set btnTests as checked by default
+        self.ui.btnTests.setChecked(True)
+    
+    def on_button_toggled(self, button, checked):
+        """Handle button toggle - ensure only one is checked at a time"""
+        if checked:
+            # Uncheck all other buttons
+            buttons = [
+                self.ui.btnTests,
+                self.ui.btnProxy,
+                self.ui.btnAuthentication,
+                self.ui.btnHTTP,
+                self.ui.btnCrawler,
+                self.ui.btnFormInputs,
+                self.ui.btnTestVectors,
+                self.ui.btnTechnologies
+            ]
+            for btn in buttons:
+                if btn != button:
+                    btn.setChecked(False)
+            
+            # Switch to corresponding tab
+            tab_map = {
+                self.ui.btnTests: 0,
+                self.ui.btnProxy: 1,
+                self.ui.btnAuthentication: 2,
+                self.ui.btnHTTP: 3,
+                self.ui.btnCrawler: 4,
+                self.ui.btnFormInputs: 5,
+                self.ui.btnTestVectors: 6,
+                self.ui.btnTechnologies: 7
+            }
+            if button in tab_map:
+                self.ui.tabWidget.setCurrentIndex(tab_map[button])
+        else:
+            # If unchecked and no other button is checked, keep this one checked
+            buttons = [
+                self.ui.btnTests,
+                self.ui.btnProxy,
+                self.ui.btnAuthentication,
+                self.ui.btnHTTP,
+                self.ui.btnCrawler,
+                self.ui.btnFormInputs,
+                self.ui.btnTestVectors,
+                self.ui.btnTechnologies
+            ]
+            if not any(btn.isChecked() for btn in buttons):
+                button.setChecked(True)
+    
+    def setup_tab_widget(self):
+        """Setup tab widget - hide the header bar"""
+        self.ui.tabWidget.tabBar().setVisible(False)
+    
+    def setup_back_button(self):
+        """Setup back button to close settingwindow and show frontwindow"""
+        self.ui.btnBack.clicked.connect(self.on_back_clicked)
+    
+    def on_back_clicked(self):
+        """Handle back button click - close settingwindow and show frontwindow"""
+        if self.main_window:
+            self.main_window.close_settingwindow_and_show_frontwindow()
+    
+    def load_stylesheet(self):
+        """Load and apply the settingwindow.qss stylesheet"""
+        file = QFile(":/Resources/settingwindow.qss")
+        if file.open(QFile.ReadOnly | QFile.Text):
+            stream = QTextStream(file)
+            stylesheet = stream.readAll()
+            self.setStyleSheet(stylesheet)
+            file.close()
